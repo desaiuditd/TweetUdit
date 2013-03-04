920f543600e23859da7f13aa297e33416b753a8c
Udit Desai <desaiuditd@gmail.com>
2013-03-05 05:06:22 +0530

    Database Migration Database configuration file + database error custom page

---
+++ TweetUdit/DBConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbName = "TweetUdit";
+?>
