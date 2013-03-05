44e8931b29b66049832912b2ca6c639e026109c6
Udit Desai <desaiuditd@gmail.com>
2013-03-05 22:41:34 +0530

    Spinner API call + initial page load ajax calls formatted

--- TweetUdit/js/main.js
+++ TweetUdit/js/main.js
@@ -2,60 +2,95 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+var opts = {
+  lines: 11, // The number of lines to draw
+  length: 19, // The length of each line
+  width: 6, // The line thickness
+  radius: 10, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  color: '#000', // #rgb or #rrggbb
+  speed: 1, // Rounds per second
+  trail: 44, // Afterglow percentage
+  shadow: true, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: 'auto', // Top position relative to parent in px
+  left: 'auto' // Left position relative to parent in px
+};
+var flag=false;
 
-
-$(document).ready(function() {
+function init() {
 
     $.post("services/getName.php",function(data) {
         $("title").append(" | "+data.name);
         $("#liName").html(data.name);
-    },"json");
 
-    $.post("services/getScreenName.php",function(data) {
-        $("#liScreenName").html("&nbsp;<strong><em><a href='home.php'>(@"+data.screenName+")</a></em></strong>");
-    },"json");
+        $.post("services/getScreenName.php",function(data) {
+            $("#liScreenName").html("&nbsp;<strong><em><a href='home.php'>(@"+data.screenName+")</a></em></strong>");
 
-    $.post("services/getProfileImageURL.php",function(data) {
-        $("#profile_pic").append("<a href='home.php'><img class='img-polaroid' src='"+data.profileImageURL+"'></a>");
-    },"json");
+            $.post("services/getProfileImageURL.php",function(data) {
+                $("#profile_pic").append("<a href='home.php'><img class='img-polaroid' src='"+data.profileImageURL+"'></a>");
 
-    $.post("services/getBackgroundColor.php",function(data) {
-        $("body").css("background-color","#"+data.bgColor);
-        $("#footer").css("background-color","#"+data.bgColor);
-    },"json");
+                $.post("services/getBackgroundColor.php",function(data) {
+                    $("body").css("background-color","#"+data.bgColor);
+                    $("#footer").css("background-color","#"+data.bgColor);
 
-    $.post("services/getSidebarColor.php",function(data) {
-        $("#wall,#followers").css("background-image","-moz-linear-gradient(top, #"+data.sbColor+", #FEFEFE)");
-        $("#wall,#followers").css("background-image","-webkit-gradient(linear, 0 0, 0 100%, to(#"+data.sbColor+"), from(#FEFEFE))");
-        $("#wall,#followers").css("background-image","-webkit-linear-gradient(top, #"+data.sbColor+", #FEFEFE)");
-        $("#wall,#followers").css("background-image","-o-linear-gradient(top, #"+data.sbColor+", #FEFEFE)");
-        $("#wall,#followers").css("background-image","linear-gradient(to bottom, #"+data.sbColor+", #FEFEFE)");
-        $("#wall,#followers").css("background-repeat","repeat-x");
-    },"json");
+                    $.post("services/getSidebarColor.php",function(data) {
+                        $("#wall,#followers").css("background-image","-moz-linear-gradient(top, #"+data.sbColor+", #FEFEFE)");
+                        $("#wall,#followers").css("background-image","-webkit-gradient(linear, 0 0, 0 100%, to(#"+data.sbColor+"), from(#FEFEFE))");
+                        $("#wall,#followers").css("background-image","-webkit-linear-gradient(top, #"+data.sbColor+", #FEFEFE)");
+                        $("#wall,#followers").css("background-image","-o-linear-gradient(top, #"+data.sbColor+", #FEFEFE)");
+                        $("#wall,#followers").css("background-image","linear-gradient(to bottom, #"+data.sbColor+", #FEFEFE)");
+                        $("#wall,#followers").css("background-repeat","repeat-x");
 
-    $.post("services/getBackgroundImageURL.php",function(data) {
-      $("body").css("background-image","url('"+data.bgImageURL+"')");
-      $("body").css("background-repeat","no-repeat");
-      $("body").css("background-position","0% -15%");
-    },"json");
+                        $.post("services/getBackgroundImageURL.php",function(data) {
+                            $("body").css("background-image","url('"+data.bgImageURL+"')");
+                            $("body").css("background-repeat","no-repeat");
+                            $("body").css("background-position","0% -15%");
 
-    $.post("services/getTweetsHomeTimeline.php",function(data) {
-        var source = $("#tmpltTweets").html();
-        var template = Handlebars.compile(source);
-        var html = template(data);
-        $("#wall h4").after(html);
-        $("#divTweets").carousel("cycle");
-    },"json");
+                            $.post("services/getTweetsHomeTimeline.php",function(data) {
+                                var source = $("#tmpltTweets").html();
+                                var template = Handlebars.compile(source);
+                                var html = template(data);
+                                $("#wall h4").after(html);
+                                $("#divTweets").carousel("cycle");
 
-    $.post("services/getFollowers.php",function(data) {
-        var source = $("#tmpltFollowers").html();
-        var template = Handlebars.compile(source);
-        var html = template(data);
-        $("#followers").append(html);
+                                $.post("services/getFollowers.php",function(data) {
+                                    var source = $("#tmpltFollowers").html();
+                                    var template = Handlebars.compile(source);
+                                    var html = template(data);
+                                    $("#followers").append(html);
+
+                                    flag = true;
+                                },"json");
+                            },"json");
+                        },"json");
+                    },"json");
+                },"json");
+            },"json");
+        },"json");
     },"json");
+}
+
+$(document).ready(function() {
+
+    var spinner = new Spinner(opts).spin(document.body);
+
+    init();
+
+    function stopSpinner() {
+        if(flag==true) {
+            spinner.stop();
+            clearInterval(interval);
+        }
+    }
+
+    var interval = setInterval(stopSpinner, 3000);
 
     $(document).on("click",".follower",function(e) {
-        $.post("services/getTweetsTimeline.php",{ screen_name : $this.html() },function(data){
+        $.post("services/getTweetsTimeline.php",{screen_name : $this.html()},function(data){
             console.log(data);
         },"json");
     });
