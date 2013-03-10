26672754ed737d264f1ddcab10cd7860054a77c9
Udit Desai <desaiuditd@gmail.com>
2013-03-10 15:28:34 +0530

    getFollowers service API added

---
+++ TweetUdit/services/getFollowers.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+session_start();
+require_once('../twitteroauth/twitteroauth.php');
+require_once('../twitteroauth/config.php');
+require_once ('../DBConfig.php');
+require_once ('../User.php');
+
+/* If access tokens are not available redirect to connect page. */
+if (empty($_SESSION['access_token']) || empty($_SESSION['access_token']['oauth_token']) || empty($_SESSION['access_token']['oauth_token_secret'])) {
+    header('Location: ../clearsession.php');
+}
+/* Get user access tokens out of the session. */
+$access_token = $_SESSION['access_token'];
+
+if(empty($_REQUEST['query'])) {
+    header('Location: ../error.html');
+}
+$query = $_REQUEST['query'];
+
+$id = $_SESSION['user'];
+
+$con = mysqli_connect($host,$username,$password,$dbName);
+if(mysqli_connect_errno($con)) {
+    header('Location: ../DBError.html');
+}
+
+$rs = mysqli_query($con,"select * from user where id='".$id."'");
+
+if($rs =  mysqli_fetch_array($rs)) {
+    $user = new User($rs);
+} else {
+    header('Location: ../DBError.html');
+}
+
+/* Create a TwitterOauth object with consumer/user tokens. */
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
+$followers = $connection->get("followers/list",array("user_id"=>$user->get_id(),"skip_status "=>true,"include_user_entities ",false));
+
+$data = array();
+
+foreach ($followers->users as $follower) {
+    echo stripos($follower->screen_name,$query);
+    if(stripos($follower->screen_name,$query)==0) {
+        $data[] = $follower->screen_name;
+    }
+}
+
+echo json_encode($data);
+?>
